d7fbf4c8cd9e7764a1d264bf541543f8880ef743
Arha Yalamanchili <yalamanchiliar@login-el8.khoury.northeastern.edu>
2026-09-24 13:22:12 -0400

    Add starter code and completed output.txt

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+<<<<<<< HEAD
+  printf("Arha\n");
+  printf("yalamanchili.ar@northeastern.edu\n");
+=======
+  printf("Hello, World!\n");
+>>>>>>> 52346efe213f59f366c3452007f876051a86679b
+
+  return 0;
+}
